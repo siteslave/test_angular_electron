422ffb6e2aa78d9f61595e2434fd1e448c36528b
Satit Rianpit <siteslave@msn.com>
2016-03-12 15:17:53 +0700

    change folder structure

---
+++ MainCtrl.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('app.controller.Main', ['app.service'])
+  .controller('MainCtrl', function ($scope, UserService) {
+    $scope.name = 'Electron JS';
+
+    $scope.showName = function () {
+      alert($scope.name)
+    };
+
+    $scope.setName = function (_name, _name2) {
+      $scope.name = _name;
+      $scope.showName();
+    };
+
+    $scope.showType = false;
+
+    $scope.departments = UserService.getDepartment();
+
+    $scope.changeDepartment = function () {
+
+      $scope.showType = true;
+      $scope.userTypes = UserService.getUserTypes($scope.departmentId);
+
+    };
+
+    $scope.changeUserType = function() {
+
+      $scope.users = UserService.getUsers($scope.typeId);
+
+    };
+
+  })
--- app.js
+++ app.js
@@ -3,36 +3,4 @@
 
 require('angular');
 
-angular.module('app', ['app.service'])
-  .controller('MainCtrl', function ($scope, UserService) {
-    $scope.name = 'Electron JS';
-
-    $scope.showName = function () {
-      alert($scope.name)
-    };
-
-    $scope.setName = function (_name, _name2) {
-      $scope.name = _name;
-      $scope.showName();
-    };
-
-    $scope.showType = false;
-
-    $scope.departments = UserService.getDepartment();
-
-    $scope.changeDepartment = function () {
-
-      //console.log('Department changed');
-      //console.log($scope.departmentId);
-      $scope.showType = true;
-      $scope.userTypes = UserService.getUserTypes($scope.departmentId);
-
-    };
-
-    $scope.changeUserType = function() {
-
-      $scope.users = UserService.getUsers($scope.typeId);
-
-    };
-
-  });
+angular.module('app', ['app.controller.Main']);
