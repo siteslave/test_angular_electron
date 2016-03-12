04fc52ee7a3f9a7ddce60ec32a5daed4d3711b81
Satit Rianpit <siteslave@msn.com>
2016-03-12 16:17:09 +0700

    add routing

--- app.js
+++ app.js
@@ -2,5 +2,35 @@
 'use strict';
 
 require('angular');
+require('angular-ui-router');
 
-angular.module('app', ['app.controller.Main']);
+angular.module('app', [
+  'ui.router',
+  'app.controller.Main'
+])
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('main', {
+      url: '/',
+      templateUrl: './templates/main.html',
+      controller: 'MainCtrl'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: './templates/about.html',
+      controller: function ($scope) {
+        $scope.name = 'Electron Application'
+      }
+    })
+    .state('detail', {
+      url: '/detail/:id/:name',
+      templateUrl: './templates/detail.html',
+      controller: function ($scope, $stateParams) {
+        console.log($stateParams);
+      }
+    })
+
+})
